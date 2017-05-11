087beccce4a0866196e4c7057ef035a797fe7224
JolitaAG <jolita278@gmail.com>
2017-05-11 11:33:14 +0300

    edited controllers and routes for admin role

--- app/Http/Controllers/PMUsersController.php
+++ app/Http/Controllers/PMUsersController.php
@@ -4,85 +4,85 @@
 
 class PMUsersController extends BaseAPIController {
 
-	/**
-	 * Display a listing of the resource.
-	 * GET /pmusers
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
+    /**
+     * Display a listing of the resource.
+     * GET /pmusers
+     *
+     * @return Response
+     */
+    public function adminIndex()
+    {
+        //
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 * GET /pmusers/create
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     * GET /pmusers/admincreate
+     *
+     * @return Response
+     */
+    public function adminCreate()
+    {
+        //
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /pmusers
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+    /**
+     * Store a newly created resource in storage.
+     * POST /pmusers
+     *
+     * @return Response
+     */
+    public function adminStore()
+    {
+        //
+    }
 
-	/**
-	 * Display the specified resource.
-	 * GET /pmusers/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     * GET /pmusers/{id}
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function adminShow($id)
+    {
+        //
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /pmusers/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     * GET /pmusers/{id}/adminedit
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function adminEdit($id)
+    {
+        //
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /pmusers/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     * PUT /pmusers/{id}
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function adminUpdate($id)
+    {
+        //
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /pmusers/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Remove the specified resource from storage.
+     * DELETE /pmusers/{id}
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function adminDestroy($id)
+    {
+        //
+    }
 
 }
