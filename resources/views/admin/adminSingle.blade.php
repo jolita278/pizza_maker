ab2e34dfe5a8e19174f1e33c48565a313ff37bd4
JolitaAG <jolita278@gmail.com>
2017-05-12 13:49:58 +0300

    added blade for single item view

--- resources/views/admin/adminSingle.blade.php
+++ resources/views/admin/adminSingle.blade.php
@@ -1,4 +1,52 @@
 @extends('admin.core')
 
-@section('content')
+@section('adminSingle')
+    <table class="table table-condensed">
+        <thead>
+        <br>
+        </thead>
+        <tbody>
+
+        @foreach($single as $key => $value)
+            <tr><td>{{$key}} </td>
+                <td> {{$value}}</td>
+        @endforeach
+
+            </tr>
+            <a href="{{route($routeEdit, $single['id'])}}">
+                <button>Koreguoti</button>
+            </a>
+            <a onclick="deleteItem('{{route($routeShow, $single['id'])}}')">
+                <button>Ištrinti</button>
+            </a>
+        </tbody>
+    </table>
+
+@endsection
+
+@section('scripts')
+
+    <script>
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        function deleteItem(route) {
+            $.ajax({
+                url: route,
+                dataType : 'json',
+                type: 'DELETE',
+                success: function () {
+                    alert('DELETED');
+                },
+                error: function () {
+                    alert('ERROR');
+                }
+            });
+        }
+
+    </script>
 @endsection
