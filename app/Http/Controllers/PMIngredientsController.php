89af07ea702c6619b88578a31f7a2cc26bdf2da5
JolitaAG <jolita278@gmail.com>
2017-05-12 14:15:22 +0300

    edited functions adminStore, adminShow, adminDestroy

--- app/Http/Controllers/PMIngredientsController.php
+++ app/Http/Controllers/PMIngredientsController.php
@@ -3,14 +3,15 @@
 
 use App\Models\PMIngredients;
 
-class PMIngredientsController extends BaseAPIController {
-
+class PMIngredientsController extends BaseAPIController
+{
 
     public function adminIndex()
     {
-        $configuration ['list']=PMIngredients::get()->toArray();
-        $configuration ['routeShow']='app.admin.ingredients.single';
-        $configuration ['routeEdit']='app.admin.ingredients.edit';
+        $configuration ['list'] = PMIngredients::get()->toArray();
+        $configuration ['routeShow'] = 'app.admin.ingredients.single';
+        $configuration ['routeEdit'] = 'app.admin.ingredients.edit';
+        $configuration ['routeNew'] = 'app.admin.ingredients.create';
         return view('admin.adminList', $configuration);
     }
 
@@ -22,7 +23,7 @@ class PMIngredientsController extends BaseAPIController {
      */
     public function adminCreate()
     {
-        //
+        return view('admin.adminIngredientsCreate');
     }
 
     /**
@@ -33,38 +34,71 @@ class PMIngredientsController extends BaseAPIController {
      */
     public function adminStore()
     {
-        //
+        $data = request()->all();
+
+        $calories = $data['calories'];
+
+        $name = $data['name'];
+
+        if ($name == null) {
+
+            $config['error'] = ['id' => 'Klaida 00002', 'message' => 'Neužpildytas laukas "Ingridiento pavadinimas" !'];
+
+            return view('admin.adminIngredientsCreate', $config);
+
+        }
+        elseif ($calories == null) {
+
+            $config['error'] = ['id' => 'Klaida 00002', 'message' => 'Neužpildytas laukas "Kalorijos"!'];
+
+            return view('admin.adminIngredientsCreate', $config);
+
+        }
+
+        PMIngredients::create(
+            [
+                'name' => $data['name'],
+                'calories' => $data['calories'],
+            ]
+        );
+
+        $config['success_message'] = ['id' => 'Įrašas sėkmingai įrašytas į DB! ', 'message' => 'Sukurtas naujas ingridientas -  ' . $data['name']];
+
+        return view('admin.adminIngredientsCreate', $config);
     }
 
     /**
      * Display the specified resource.
      * GET /pmingredients/{id}
      *
-     * @param  int  $id
+     * @param  int $id
      * @return Response
      */
     public function adminShow($id)
     {
-        return view('admin.adminSingle');
+        $configuration ['single'] = PMIngredients::find($id)->toArray();
+        $configuration ['routeShow'] = 'app.admin.ingredients.single';
+        $configuration ['routeEdit'] = 'app.admin.ingredients.edit';
+        return view('admin.adminSingle', $configuration);
     }
 
     /**
      * Show the form for editing the specified resource.
      * GET /pmingredients/{id}/adminedit
      *
-     * @param  int  $id
+     * @param  int $id
      * @return Response
      */
     public function adminEdit($id)
     {
-        return view('admin.adminSingle');
+        return view('admin.core');
     }
 
     /**
      * Update the specified resource in storage.
      * PUT /pmingredients/{id}
      *
-     * @param  int  $id
+     * @param  int $id
      * @return Response
      */
     public function adminUpdate($id)
@@ -76,11 +110,14 @@ class PMIngredientsController extends BaseAPIController {
      * Remove the specified resource from storage.
      * DELETE /pmingredients/{id}
      *
-     * @param  int  $id
-     * @return Response
+     * @param  int $id
+     * @return mixed
      */
     public function adminDestroy($id)
     {
+        PMIngredients::destroy($id);
 
+        return json_encode(["success" => true, "id" => $id]);
+//TODO:: refresh VIEW
     }
 }
