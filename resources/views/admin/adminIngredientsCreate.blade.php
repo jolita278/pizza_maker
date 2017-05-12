2a6a31b2bcc1e2515e0eb54d0b5e06bc0922ab2e
JolitaAG <jolita278@gmail.com>
2017-05-12 14:13:03 +0300

    created blade for ingredients creating form

---
+++ resources/views/admin/adminIngredientsCreate.blade.php
@@ -0,0 +1,35 @@
+@extends('admin.core')
+
+@section('adminIngredientsCreate')
+    @if(isset($success_message))
+
+        @foreach($success_message as $message)
+            <div style="background:green; color:white"> {{$message}}!</div>
+        @endforeach
+    @endif
+
+    @if(isset($error))
+
+        @foreach($error as $err)
+            <div style="background:red; color:white"> {{$err}}!</div>
+        @endforeach
+
+    @endif
+
+
+    {!! Form::open(['url' => route('app.admin.ingredients.create')]) !!}
+    <br>
+    {{ Form::label('name', 'Ingridiento pavadinimas')}}<br>
+    {{Form::text('name')}}
+
+    <br>
+    {{ Form::label('calories', 'Kalorijos')}}<br>
+    {{Form::text('calories')}}
+
+    <br>
+    <br>
+
+    {{ Form::submit('Patvirtinti') }} {{--TODO:: button reset--}}
+
+    {!! Form::close() !!}
+@endsection
